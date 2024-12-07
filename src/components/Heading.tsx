c3a37b238c0d0e2c922d48b07429712570fbbf2e
Tasfiq Sunny <tasfiq.asif.23@gmail.com>
2024-12-07 08:21:51 +0600

    primary folder structure

---
+++ src/components/Heading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <div>
+            <h1>
+            This is my heading    
+            </h1>        </div>
+    );
+};
+
+export default Heading;
